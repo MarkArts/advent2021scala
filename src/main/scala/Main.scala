944a7cace35bc47364dd6b9765bb58f6ba0f9047
Mark Arts <mark@gutstickets.org>
2021-12-02 18:34:57 +0100

    expand readme

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,7 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-@main def bye: Unit = 
-  println("bye world!")
-  println(msg)
-
-
-def msg = "I was compiled by Scala 3. :)"
+@main def day1: Unit = 
+  val filename = "day1.txt"
+  var inputs = Source.fromFile(filename).getLines.map(l => l.toInt).toList
+  
+  println(countIncreased(inputs))
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,6 +1,16 @@
 import org.junit.Test
 import org.junit.Assert.*
 
-class Test1:
-  @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", msg)
+class Day1:
+  @Test def test_steps(): Unit =
+    var input = List(1,2,3,4,5)
+    assertEquals(List(Step(1,2), Step(2,3), Step(3,4), Step(4,5)), createSteps(input))
+  @Test def test_countIncreased(): Unit = 
+    var input = List(199, 200, 208, 210 ,200, 207, 240, 269, 260, 263)
+    assertEquals(7, countIncreased(input))
+  @Test def test_countIncreased_empty(): Unit = 
+    var input = List()
+    assertEquals(0, countIncreased(input))
+  @Test def test_countIncreased_none(): Unit = 
+    var input = List(10,9,8,7)
+    assertEquals(0, countIncreased(input))
